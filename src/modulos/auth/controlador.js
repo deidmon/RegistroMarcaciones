1ba8cab760c72ef917d785464ca444ddc08bef22
deidmon <paul.dedios@alum.udep.edu.pe>
2023-09-15 15:51:34 -0500

    Auth eliminar comentarios

--- src/modulos/auth/controlador.js
+++ src/modulos/auth/controlador.js
@@ -20,19 +20,13 @@ module.exports = function(dbInyectada){
               }
               return bcrypt.compare(password, data.Contraseña)
               .then(resultado =>{
-                if(resultado){//resultado == true
+                if(resultado){
                       const payload = {
-                            /* IdUsuarios:data.IdUsuarios,
-                            Nombres: data.Nombres,
-                            Apellidos: data.Apellidos,
-                            Usuario: data.Usuario, */
                             randomData: Math.random(),
-                        // otherRandomValue: 'SomeRandomValue',
-                        //exp: Math.floor(Date.now() / 1000) + 3600,
-                        exp: Math.floor(Date.now() / 1000) + 3600,
+                            exp: Math.floor(Date.now() / 1000) + 3600,
         
                       };
-                      // const token = auth.asignarToken(payload, SECRET_KEY, { expiresIn: '1h' });
+                     
                       return auth.asignarToken({...payload})
                 }else{
                       throw new Error("Credenciales inválidas");
