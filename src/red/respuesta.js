4c72db17a5d522a0589564012914d30b74138237
deidmon <paul.dedios@alum.udep.edu.pe>
2023-10-03 16:14:48 -0500

    Modificar body

--- src/red/respuesta.js
+++ src/red/respuesta.js
@@ -1,16 +1,18 @@
 exports.success = function(req, res, mensaje = '', status = 200){
     res.status(status).send({
-        error: false,
-        status: status,
-        body: mensaje
+        success: false,
+        status_code: status,
+        status_message: 'Succesfully',
+        data: mensaje
     });
 }
 exports.error = function(req, res, mensaje, status){
     const statusCode = status || 500;
     const mensajeError = mensaje || 'Error interno';
     res.status(statusCode).send({ 
-        error: true,
-        status: statusCode,
-        body: mensajeError
+        success: true,
+        status_code: statusCode,
+        status_message: mensajeError,
+        data: {}
     });
 }
