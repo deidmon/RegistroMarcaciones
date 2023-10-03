847f2965989e5b7d6595bad53ed69f06d10aa72f
deidmon <paul.dedios@alum.udep.edu.pe>
2023-10-03 18:25:36 -0500

    Modificar success

--- src/red/respuesta.js
+++ src/red/respuesta.js
@@ -1,6 +1,6 @@
 exports.success = function(req, res, mensaje = '', status = 200){
     res.status(status).send({
-        success: false,
+        success: true,
         status_code: status,
         status_message: 'Succesfully',
         data: mensaje
@@ -10,7 +10,7 @@ exports.error = function(req, res, mensaje, status){
     const statusCode = status || 500;
     const mensajeError = mensaje || 'Error interno';
     res.status(statusCode).send({ 
-        success: true,
+        success: false,
         status_code: statusCode,
         status_message: mensajeError,
         data: {}
